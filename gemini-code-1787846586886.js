636a91051161fede3ab5ada7f5429635f869bb05
thalytaroberta <thalytarobertadasilva@gmail.com>
2026-08-27 13:03:32 -0300

    Add files via upload

---
+++ gemini-code-1787846586886.js
@@ -0,0 +1,6 @@
+<h1 style={{ fontSize: '2.5rem', fontWeight: 'bold' }}>
+  <span style={{ color: '#002B7F' }}>Edu</span>
+  <span style={{ color: '#FFC72C' }}>Braille</span>
+  <span style={{ color: '#CE1126' }}> - </span>
+  <span style={{ color: '#009739' }}>PE</span>
+</h1>
